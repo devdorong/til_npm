049a25cb066c6bd7c3db7b7fdd1a35f046cf4bb0
devdorong <dev.dorong@gmail.com>
2025-07-28 13:18:48 +0900

    [docs] Date 라이브러리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,10 @@ export default defineConfig([
       prettier: pluginPrettier,
     },
     rules: {
-      ...pluginReact.configs.recommended.rules,
+      // react 추천 규칙 중에서 필요하면 수동 설정
+      // ...pluginReact.configs.recommended.rules, // ❌ 삭제 권장
+      "react/react-in-jsx-scope": "off", // ✅ 명확하게 선언
+      "react/prop-types": "off",
       "prettier/prettier": "warn",
       "no-var": "warn",
       "no-unused-vars": "warn",
