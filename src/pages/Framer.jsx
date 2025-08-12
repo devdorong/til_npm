54647a74180b34cf352d33b4fe10649d17c50e06
devdorong <dev.dorong@gmail.com>
2025-08-12 12:32:18 +0900

    [docs] framer motion 기본 활용

--- src/pages/Framer.jsx
+++ src/pages/Framer.jsx
@@ -1,23 +1,3 @@
-# Framer Motion
-
-- https://motion.dev/docs/react
-- https://examples.motion.dev/react
-- 참조 사이트
-  - https://velog.io/@keumky1/Framer-Motion-입문하기
-  - https://nykim.work/114
-
-- 설치
-
-```bash
-npm install motion
-npm i framer-motion
-```
-
-## 폴더 및 파일 구조
-
-- /src/pages/Framer.jsx
-
-```jsx
 import { motion } from "framer-motion";
 function Framer() {
   return (
@@ -71,4 +51,3 @@ function Framer() {
 }
 
 export default Framer;
-```
